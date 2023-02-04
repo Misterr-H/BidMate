a83293a13bbfacc3827e3e90427c76cf76794dbf
somay <ssomay2002@gmail.com>
2023-02-05 04:53:55 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ import {useState} from "react";
 import Lender from "@/containers/Lender";
 import Borrower from "@/containers/Borrower";
 import Navbar from "@/components/Navbar";
-import LandingPage from "@/containers/LandingPage";
 
 export default function Home() {
     const [value, setValue] = useState('1');
@@ -20,7 +19,6 @@ export default function Home() {
           <Head>
             <title>BidMate</title>
           </Head>
-          {/*<LandingPage/>*/}
           <Navbar/>
           <TabContext value={value}>
                 <TabList onChange={handleChange} aria-label="simple tabs example">
