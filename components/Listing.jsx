4b700e0f8ee88e772338941225011d95fab4e8ad
somay <ssomay2002@gmail.com>
2023-02-05 03:22:09 +0530

    commit

--- components/Listing.jsx
+++ components/Listing.jsx
@@ -1,13 +1,16 @@
 const Listing = ({name, creditScore, amount, time, rate}) => {
     return (
-        <div className={'shadow flex bg-white w-80 space-between hover:bg-gray-100 cursor-pointer justify-between'}>
+        <div className={'shadow my-4 flex bg-white w-80 space-between hover:bg-gray-100 cursor-pointer justify-between'}>
             <div className={'flex flex-col justify-center items-center p-4'}>
                 <h1 className={'text-2xl'}>{name}</h1>
                 <h1 className={`text-xl ${creditScore < 670 ? 'text-red-600' : creditScore < 740 ? 'text-yellow-400' : 'text-green-600'}`}>{creditScore}</h1>
             </div>
-            <div className={'flex flex-col justify-center items-center text-cyan-600 p-4'}>
+            <div className={'flex w-80 flex-col justify-center items-center text-cyan-600 p-3 ml-10'}>
                 <h1 className={'text-2xl font-bold'}>₹{amount}</h1>
                 <h1 className={'text-xl'}>{time} Months</h1>
+                {/*<h1 className={'text-xl'}>{rate}%</h1>*/}
+            </div>
+            <div className={'flex flex-col items-center text-cyan-600 p-4'}>
                 <h1 className={'text-xl'}>{rate}%</h1>
             </div>
         </div>
--- containers/Lender.jsx
+++ containers/Lender.jsx
@@ -5,6 +5,8 @@ const Lender = ({name}) => {
         <div>
             <h1>Hi {name}, available categories for Lending Money</h1>
             <Listing name={'John'} creditScore={800} amount={10000} time={12} rate={10}/>
+            <Listing name={'John'} creditScore={800} amount={10000} time={12} rate={10}/>
+            <Listing name={'John'} creditScore={800} amount={10000} time={12} rate={10}/>
         </div>
     )
 }
